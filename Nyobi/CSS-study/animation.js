4749e5906388b865df5c7b71bbe5ed94113ffe73
ibuki-taguchi <ibuki-taguchi@cybozu.co.jp>
2021-08-11 10:39:48 +0900

    css-study

--- Nyobi/CSS-study/animation.js
+++ Nyobi/CSS-study/animation.js
@@ -1,2 +1,14 @@
 let header = document.getElementById("header");
-header.style.transform = "rotateX(60deg)";
+let degree = 0;
+function rotateHeader() {
+  degree = degree + 6;
+  degree = degree % 360;
+  if ((0 <= degree && degree <= 90) || (270 <= degree && degree <= 360)) {
+    header.className = "face";
+  } else {
+    header.className = "back";
+  }
+  header.style.transform = `rotateX(${degree}deg)`;
+}
+
+setInterval(rotateHeader, 20);
